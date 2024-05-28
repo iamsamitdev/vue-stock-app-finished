8eccc6f7c2ccdd7fbd74387dcafaaf004742acd5
Samit Koyom <samitkoyom@gmail.com>
2024-05-28 17:37:45 +0700

    initial commit

---
+++ src/layouts/FrontendLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import FrontendNav from '@/components/frontend/FrontendNav.vue'
+    import FrontendFoot from '@/components/frontend/FrontendFoot.vue'
+</script>
+
+<template>
+    <div>
+        <FrontendNav />
+        <main>
+            <router-view />
+        </main>
+        <FrontendFoot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/services/AuthService.ts
@@ -0,0 +1,60 @@
+import axios from "axios"
+
+// อ่าน token จาก localStorage
+const token = localStorage.getItem('token') || ''
+
+const authService = axios.create({
+  baseURL: import.meta.env.VITE_APP_URL_API,
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+const backendService = axios.create({
+    baseURL: import.meta.env.VITE_APP_URL_API,
+    withCredentials: false,
+    headers: {
+      'Content-Type':'application/json',
+      'Authorization': 'Bearer ' + token // ใส่ token ไปด้วยก่อนส่งไปที่ backend ทุกครั้ง
+    }
+})
+
+// Login
+function login(email: string, password: string) {
+  return authService.post("login", {
+    email: email,
+    password: password,
+  })
+}
+
+// Register
+function register(
+  fullname: string, 
+  username: string, 
+  email: string, 
+  password: string, 
+  password_confirmation: string, 
+  tel: string, 
+  role: string) {
+  return authService.post("register", {
+    fullname: fullname,
+    username: username,
+    email: email,
+    password: password,
+    password_confirmation: password_confirmation,
+    tel: tel,
+    role: role
+  })
+}
+
+// Logout
+function logout() {
+  return backendService.post("logout")
+}
+
+export {
+  login,
+  register,
+  logout
+}
